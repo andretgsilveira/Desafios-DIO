448a4357dbb0c5f2cfa21606efb34f81e28cb9d9
andretgsilveira <andre_tgsilveira@hotmail.com>
2022-03-04 16:52:29 -0300

    Criação da classe Experiencias

---
+++ src/desafiosIntermediarios/Experiencias.java
@@ -0,0 +1,30 @@
+package desafiosIntermediarios;
+
+import java.util.*;
+
+public class Experiencias {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int quantidade = 0;
+        String tipo;
+        int cobaias;
+
+        quantidade = input.nextInt();
+
+        Dictionary<Integer, String> dic = new Hashtable<Integer, String>();
+
+        for (int i = 0; i < quantidade; i++){
+            cobaias = input.nextInt();
+            tipo = input.next();
+
+            dic.put(tipo, )
+
+
+        }
+
+        System.out.println(lista.get(2));
+
+
+    }
+}
