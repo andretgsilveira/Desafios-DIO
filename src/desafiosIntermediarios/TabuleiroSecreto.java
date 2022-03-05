5453cae996d9a310677e89a2e06bef2b4f038329
andretgsilveira <andre_tgsilveira@hotmail.com>
2022-03-05 19:26:33 -0300

    Concluido codigo TabuleiroSecreto

--- src/desafiosIntermediarios/TabuleiroSecreto.java
+++ src/desafiosIntermediarios/TabuleiroSecreto.java
@@ -1,70 +1,90 @@
 package desafiosIntermediarios;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TabuleiroSecreto {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
 
-        int tabuleiro = scan.nextInt();
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        String[] nq = buffer.readLine().split(" ");
+
+        int tabuleiro = Integer.parseInt(nq[0]);
+        int jogadas = Integer.parseInt(nq[1]);
 
         int m[][] = new int[tabuleiro][tabuleiro];
-        for (int x = 0; x < m.length; x++){
-            for (int y = 0; y < m[x].length; y++){
+        int auxArray[] = new int[tabuleiro];
+
+        for (int x = 0; x < m.length; x++) {
+            for (int y = 0; y < m[x].length; y++) {
                 m[x][y] = 0;
             }
         }
 
-        int jogadas = scan.nextInt();
         int x = 0, r = 0, j = 0, cont = 0;
 
 
-        for (int i = 0; i < jogadas; i++){
-            int operacao = scan.nextInt();
-            switch (operacao){
+        for (int i = 0; i < jogadas; i++) {
+            String[] entrada = buffer.readLine().split(" ");
+            switch (Integer.parseInt(entrada[0])) {
                 case 1:
-                    x = scan.nextInt();
-                    r = scan.nextInt();
-                    for (j = 0; j < m.length; j++){
-                        m[x-1][j] = r;
+                    x = Integer.parseInt(entrada[1]);
+                    r = Integer.parseInt(entrada[2]);
+                    for (j = 0; j < m.length; j++) {
+                        m[x - 1][j] = r;
                     }
                     break;
                 case 2:
-                    x = scan.nextInt();
-                    r = scan.nextInt();
-                    for (j = 0; j < m.length; j++){
-                        m[j][x-1] = r;
+                    x = Integer.parseInt(entrada[1]);
+                    r = Integer.parseInt(entrada[2]);
+                    for (j = 0; j < m.length; j++) {
+                        m[j][x - 1] = r;
                     }
                     break;
                 case 3:
-                    x = scan.nextInt();
-
-                    for(i = 0; i < m.length; i++){
-                        for(j = 0; j < m.length; j++){
-                            if(m[i][x-1] == m[j][x-1]){
-                                cont++;
-                            }
-                        }
-
+                    x = Integer.parseInt(entrada[1]);
+                    for (j = 0; j < m.length; j++) {
+                        auxArray[j] = m[x-1][j];
                     }
-
-
-
-
+                    System.out.println(duplicado(auxArray));
                     break;
                 case 4:
-                    x = scan.nextInt();
-
+                    x = Integer.parseInt(entrada[1]);
+                    for (j = 0; j < m.length; j++) {
+                        auxArray[j] = m[j][x-1];
+                    }
+                    System.out.println(duplicado(auxArray));
                     break;
             }
 
+
         }
 
-//        for (x = 0; x < m.length; x++){
-//            for (int y = 0; y < m[x].length; y++){
-//                System.out.print(m[x][y]);
-//            }
-//            System.out.println();
-//        }
+    }
+
+    public static int duplicado(int arr[]) {
+        int numero = 0;
+        int numeroAux = 0;
+        int cont = 0;
+        int contAux = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    numeroAux = arr[i];
+                    cont++;
+                }
+            }
+            if (cont > contAux) {
+                contAux = cont;
+                numero = numeroAux;
+            } else if (cont == contAux && numeroAux > numero) {
+                contAux = cont;
+                numero = numeroAux;
+            }
+            cont = 0;
+        }
+        return numero;
     }
 }
+
